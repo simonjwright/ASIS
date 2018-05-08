fe4b44ea64ddc22db00280ef4d5f97bb152db3b4
Simon Wright <simon@pushface.org>
2018-05-08 16:46:25 +0100

    Changes required to compile with FSF GCC 8.1.0.
      * Makefile (setup-snames): dummied out, since we get GNAT internals
          from gnat_util.
      * asis.gpr (Source_Dirs): remove "gnat".
      * asis/a4g-gnat_int.adb (Tree_In_With_Version_Check): remove the
          Strong_Version_Check code, which is incompatible with FSF GCC
          version strings.
      * asis/asis-clauses.adb (Clause_Names): remove variable
          Result_List. Rework handling of Use_Type clauses vs Use Package
          to match compiler internals.
      * common.gpr: with's gnat_util.
        (Ada_Strict_Switches): change -gnatwae to -gnatwa, there are some
          warnings, don't make them errors. Add -gnatqQ to keep ALI even
          if there's a compilation error.
      * .gitignore: new.

--- asis/a4g-gnat_int.adb
+++ asis/a4g-gnat_int.adb
@@ -233,49 +233,6 @@ package body A4G.GNAT_Int is
 
       Opt.Tree_Read;
 
-      if Strong_Version_Check then
-         --  We check only the dates here!
-         First_A_Idx :=
-           Index (Source  => ASIS_GNAT_V,
-                  Pattern => "(") + 1;
-
-         First_T_Idx :=
-           Index (Source  => Tree_Version_String.all,
-                  Pattern => "(") + 1;
-
-         Last_A_Idx := Index (Source  => ASIS_GNAT_V,
-                     Pattern => ")") - 1;
-
-         if Index
-              (Source  => ASIS_GNAT_V (First_A_Idx .. Last_A_Idx),
-               Pattern => "-") /= 0
-         then
-            Last_A_Idx :=
-              Index (Source  => ASIS_GNAT_V (First_A_Idx .. Last_A_Idx),
-                     Pattern => "-") - 1;
-         end if;
-
-         Last_T_Idx := Index (Source  => Tree_Version_String.all,
-                              Pattern => ")") - 1;
-
-         if Index
-              (Source  => Tree_Version_String.all (First_T_Idx .. Last_T_Idx),
-               Pattern => "-") /= 0
-         then
-            Last_T_Idx :=
-              Index (Source  =>
-                       Tree_Version_String.all (First_T_Idx .. Last_T_Idx),
-                     Pattern => "-") - 1;
-         end if;
-
-         if ASIS_GNAT_V (First_A_Idx .. Last_A_Idx) /=
-            Tree_Version_String (First_T_Idx .. Last_T_Idx)
-         then
-            Note_Version_Mismatch;
-         end if;
-
-      end if;
-
       --  GNAT/ASIS version check
 
       if Tree_ASIS_Version_Number /= Tree_IO.ASIS_Version_Number then
